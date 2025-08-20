449634a05b8cece1f061521910c34ab922b9ea9f
Bruno Giacometti <152458112+chaskibum@users.noreply.github.com>
2025-08-20 20:22:12 -0300

    sin bugs

--- eventos.js
+++ eventos.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     div.addEventListener("click", function() {
         alert("Hola! Soy el terror de Suany... digo, soy el div");
     });
-});
 
-function showAlert() {
-    alert("Hola!")
-}
+    let boton = document.getElementById("boton");
+    boton.addEventListener("click", (event) => {
+        event.stopPropagation();
+        alert("Hola!")
+    });
+});
