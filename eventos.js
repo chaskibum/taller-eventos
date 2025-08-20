399064fa28b82c26264d72397c1650c12a423af8
Bruno Giacometti <152458112+chaskibum@users.noreply.github.com>
2025-08-20 19:47:54 -0300

    El div saluda

---
+++ eventos.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let div = document.getElementById("elterror");
+    div.addEventListener("click", function() {
+        alert("Hola! Soy el terror de Suany... digo, soy el div");
+    });
+});
+
+function showAlert() {
+    alert("Hola!")
+}
